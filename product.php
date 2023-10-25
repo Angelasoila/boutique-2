be596b1abe6225c254204b16b05bda5e60eadfec
Angelasoila <103871988+Angelasoila@users.noreply.github.com>
2023-10-25 03:43:53 +0300

    Add files via upload

---
+++ product.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <title>Angela's||Products</title>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="style3.css">
+    <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Chilanka&family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <!--Lightslider css-->
+    <link rel="stylesheet" type="text/css" href="lightslider.css">
+    <!--Jquery js-->
+    <script type="text/javascript" src="jquery.js"></script>
+    <!--Lightslider js-->
+    <script type="text/javascript" src="lightslider.js"></script>
+
+</head>
+<body>
+    
+    <!--navigation section-->
+    <div class="full-page">
+        <!--Navigation section-->
+       <div class='navbar'>
+            <div class='logo'>
+                <a href='#'><h1>Angela's</h1></a>
+            </div>
+            <nav>
+                <ul id='MenuItems'>
+                    <li><a href='app.php'>Home</a></li>
+                    <li><a href='product.php'>Products</a></li>
+                    <li><a href='contact.php'>Contact Us</a></li>
+                    <i class="fa fa-shopping-cart" href="#" style="margin-right:20px; color:coral; cursor:pointer;"></i>
+                    <li><a href="logout.php"><button>logout</button></a></li>
+                </ul>
+            </nav>
+       </div>
+        <!--End of navigation-->
+
+
+        <!--Slider section-->
+        <section class="slider">
+        <ul id="autoWidth" class="cs-hidden">
+
+           <!--1---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="crotchet1.jpg" alt="1">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">Crotchet Bikini Set</a>
+                        <span>All sizes available</span>
+                    </div>
+                    <a href="#" class="price">$23</a>
+                </div>
+            </div>
+           </li>
+
+           <!--2---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="pants.jpg" alt="2">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">Grey Nike Pants</a>
+                        <span>All sizes available</span>
+                    </div>
+                    <a href="#" class="price">$15</a>
+                </div>
+            </div>
+           </li>
+
+           <!--3---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="top2.jpg" alt="3">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">Casual Women Bra</a>
+                        <span>All sizes available</span>
+                    </div>
+                    <a href="#" class="price">$12</a>
+                </div>
+            </div>
+           </li>
+
+           <!--4---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="blouse.jpg" alt="4">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">Ivory Female Shirt</a>
+                        <span>New Arrival</span>
+                    </div>
+                    <a href="#" class="price">$18</a>
+                </div>
+            </div>
+           </li>
+
+           <!--5---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="glasses2.jpg" alt="5">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">White Dark Sunglasses</a>
+                        <span>New Arrival</span>
+                    </div>
+                    <a href="#" class="price">$10</a>
+                </div>
+            </div>
+           </li>
+
+           <!--6---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="dress.jpg" alt="6">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">Off-Shoulder Dress</a>
+                        <span>Black, Yellow, Blue, Red</span>
+                    </div>
+                    <a href="#" class="price">$30</a>
+                </div>
+            </div>
+           </li>
+
+           <!--7---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="pants2.jpg" alt="7">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">High-waist Pants</a>
+                        <span>All sizes available</span>
+                    </div>
+                    <a href="#" class="price">$20</a>
+                </div>
+            </div>
+           </li>
+
+           <!--8---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="blackjacket.jpg" alt="8">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">Black Leather Jacket</a>
+                        <span>All sizes available</span>
+                    </div>
+                    <a href="#" class="price">$27</a>
+                </div>
+            </div>
+           </li>
+
+           <!--9---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="sports.jpg" alt="9">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">Sports Pants</a>
+                        <span>All sizes available</span>
+                    </div>
+                    <a href="#" class="price">$16</a>
+                </div>
+            </div>
+           </li>
+
+           <!--10---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="neck.jpg" alt="10">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">Gold Necklace</a>
+                        <span>New Arrival</span>
+                    </div>
+                    <a href="#" class="price">$25</a>
+                </div>
+            </div>
+           </li>
+
+           <!--11---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="shorts.jpg" alt="11">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">Jeans Shorts</a>
+                        <span>All sizes available</span>
+                    </div>
+                    <a href="#" class="price">$8</a>
+                </div>
+            </div>
+           </li>
+
+           <!--12---------->
+           <li class="item-a">
+            <!--Box slider-->
+           <div class="box">
+               <div class="slide-img">
+                   <img src="cord.jpg" alt="12">
+                   <div class="overlay">
+                       <a href="#" class="buy-btn">Buy Now</a>
+                    </div>
+                </div>
+                <div class="detail-box">
+                    <div class="type">
+                        <a href="#">Blue Jeans</a>
+                        <span>All sizes available</span>
+                    </div>
+                    <a href="#" class="price">$23</a>
+                </div>
+            </div>
+           </li>
+
+        </ul>
+        </section>
+        <!--End of Slider section-->
+
+
+        <!--Footer section-->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>Angela's</h4>
+                    <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Our Products</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Affiliate </a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Get Help</h4>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Shipping</a></li>
+                        <li><a href="#">Order Status</a></li>
+                        <li><a href="#">Payment Options</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Online Shop</h4>
+                    <ul>
+                        <li><a href="#">Watch</a></li>
+                        <li><a href="#">Sunglasses</a></li>
+                        <li><a href="#">Jeans</a></li>
+                        <li><a href="#">Shoes</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Follow Us</h4>
+                    <div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>   
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--End of footer section-->
+    </div>
+    
+
+    
+
+
+
+    
+
+
+    <!--Javascript file-->
+    <script type="text/javascript" src="javascript.js"></script>
+<body>
+</html>
